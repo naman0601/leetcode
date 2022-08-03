a0e64fec67f4eccf541c76b89728871f26d2dd1e
naman0601 <73053395+naman0601@users.noreply.github.com>
2022-08-03 22:25:58 +0530

    Time: 549 ms (34.72%), Space: 134.6 MB (91.95%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -5,10 +5,10 @@ public:
         deque<int> dq;
         vector<int> ans;
         for(int i=0;i<nums.size();i++){
-            if(!dq.empty() && dq.front()==i-k)
+            if(!dq.empty() && dq.front()==i-k) //to pop the index of value which is not in the window 
                 dq.pop_front();
-            while(!dq.empty() && nums[dq.back()]<=nums[i])
-                dq.pop_back();
+            while(!dq.empty() && nums[dq.back()]<=nums[i]) //to pop the index of value which are less
+                dq.pop_back();         //than the max value of that window
             dq.push_back(i);
             if(i>=k-1) ans.push_back(nums[dq.front()]);
         }
