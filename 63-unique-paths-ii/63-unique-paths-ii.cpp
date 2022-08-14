8de61179267fe7dcbebc8454849a6ec322d70a5b
naman0601 <73053395+naman0601@users.noreply.github.com>
2022-08-14 11:54:14 +0530

    Time: 7 ms (40.22%), Space: 7.9 MB (12.72%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -8,7 +8,21 @@ public:
         return dp[r][c]=f(v,r-1,c,dp)+f(v,r,c-1,dp);
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        vector<vector<int>> dp(obstacleGrid.size(),vector<int>(obstacleGrid[0].size(),-1));
-        return f(obstacleGrid,obstacleGrid.size()-1,obstacleGrid[0].size()-1,dp);
+        int m=obstacleGrid.size(),n=obstacleGrid[0].size();
+        vector<vector<int>> dp(obstacleGrid.size(),vector<int>(obstacleGrid[0].size(),0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j]!=1){
+                if(i==0 && j==0) dp[i][j]=1;
+                else {
+                    int up=0,left=0; 
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
