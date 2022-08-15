8708bcd101f93acb2c60250f0829f851ddb766e4
naman0601 <73053395+naman0601@users.noreply.github.com>
2022-08-15 12:53:55 +0530

    Time: 20 ms (28.30%), Space: 10.3 MB (6.02%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minSumPathUtil(int i, int j,vector<vector<int>> &matrix,vector<vector<int>> &dp)
+{
+  if(i==0 && j == 0)
+    return matrix[0][0];
+  if(i<0 || j<0)
+    return 1e9;
+  if(dp[i][j]!=-1) return dp[i][j];
+    
+  int up = matrix[i][j]+minSumPathUtil(i-1,j,matrix,dp);
+  int left = matrix[i][j]+minSumPathUtil(i,j-1,matrix,dp);
+  
+  return dp[i][j] = min(up,left);
+  
+}
+   
+    int minPathSum(vector<vector<int>>& grid) {
+        int m=grid.size(),n=grid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return minSumPathUtil(m-1,n-1,grid,dp);
+    }
+};
