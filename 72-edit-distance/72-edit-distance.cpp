739e2e1dfa9080fad7e17b663df925c3825d0603
naman0601 <73053395+naman0601@users.noreply.github.com>
2022-08-19 14:51:32 +0530

    Time: 163 ms (10.63%), Space: 80.5 MB (12.85%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    
-    int minDistance(string word1, string word2) {
-        int n=word1.length(),m=word2.length();
-        vector<int> prev(m+1,0);
+    int f(int i,int j,string s,string t,vector<vector<int>> &dp){
+       
+        if(j<0) return i+1; //delete 
         
-        for(int j=1;j<=m;j++) 
-            prev[j]=j;         //insert
+        if(i<0)  return j+1; //insert
         
-        for(int i=1;i<=n;i++){
-            vector<int> cur(m+1,0);
-            cur[0]=i;
-            for(int j=1;j<=m;j++){
-                 if(word1[i-1]==word2[j-1])
-                    cur[j]=0+prev[j-1];
-                 else
-                    cur[j]=1+min(min(cur[j-1],prev[j]),prev[j-1]);
-            }
-            prev=cur;
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        if(s[i]==t[j])
+            return dp[i][j]=0+f(i-1,j-1,s,t,dp);
+        else{
+            int insert=1+f(i,j-1,s,t,dp);
+            int del=1+f(i-1,j,s,t,dp);
+            int replace=1+f(i-1,j-1,s,t,dp);
+            return dp[i][j]=min(min(insert,del),replace);
         }
         
-        return prev[m];
+    }
+    int minDistance(string word1, string word2) {
+        int n=word1.length(),m=word2.length();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,word1,word2,dp);
     }
 };
